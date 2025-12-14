54a35213fa3f3a2fcec43b617821bee7254a7790
aastha garg <gargaastha811@gmail.com>
2025-12-15 04:40:05 +0530

    Added LeetCode SQL Day 18 (197)

---
+++ DAY 18 (197).sql
@@ -0,0 +1,23 @@
+-- 197 RISING TEMPERATURE
+
+-- Write a solution to find all dates' id with higher temperatures compared to its previous dates (yesterday).
+
+CREATE TABLE Weather (
+    id INT,
+    recordDate DATE,
+    temperature INT
+);
+insert into Weather values
+(1,'2015-01-01',10),
+(2,'2015-01-02',25),
+(3,'2015-01-03',20),
+(4,'2015-01-04',30);
+
+SELECT 
+    w1.id
+FROM
+    Weather w1
+LEFT JOIN
+    weather w2 ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE
+    w1.temperature > w2.temperature;
